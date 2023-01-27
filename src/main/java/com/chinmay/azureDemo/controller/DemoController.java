3ac9002a2e9fbd8d1354c06da19e72124007a5d2
Chinmay Vernekar <chinmay.vernekar49@gmail.com>
2023-01-27 20:31:52 +0530

    Initial-Commit

--- src/main/java/com/chinmay/azureDemo/controller/DemoController.java
+++ src/main/java/com/chinmay/azureDemo/controller/DemoController.java
@@ -1,9 +1,10 @@
 package com.chinmay.azureDemo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class DemoController {
 
     @GetMapping("/test")

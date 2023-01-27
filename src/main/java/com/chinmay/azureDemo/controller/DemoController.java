b66638eb2216dc00f9479d21ae900f0fef15ea7a
Chinmay Vernekar <chinmay.vernekar49@gmail.com>
2023-01-27 20:53:21 +0530

    Initial-Commit

--- src/main/java/com/chinmay/azureDemo/controller/DemoController.java
+++ src/main/java/com/chinmay/azureDemo/controller/DemoController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class DemoController {
 
-    @GetMapping("/test")
+    @GetMapping("/")
     public String testAzure(){
         return "HELLO USER";
     }
